e539e7a164da1e3bd3d30ca785eb6443b046d97e
Hauphvn <hauphvn@gmail.com>
2019-03-21 14:24:16 +0700

    test02

--- tests/test001.py
+++ tests/test001.py
@@ -1,6 +1,20 @@
 import unittest
 
+from testee.services.add import add
+
 
 class TestAdd(unittest.TestCase):
+
     def test_01(self):
         print('hello')
+
+    def test_02(self):
+        # input
+        a = 1
+        b = 2
+
+        # get actual output
+        result = add(a,b)
+
+        # check expected output
+        assert result == 3
