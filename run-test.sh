27260999fb1757772fc6dde615ca136981c9abe5
NN <namgivu@gmail.com>
2019-03-21 18:47:06 +0700

    run test all or only-google

--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"
 
+only_google_test=$1
+
 export PYTHONPATH="$SCRIPT_HOME:$PYTHONPATH"
 
-#                 #of worker  #show elapsed                     #turn off warning         #specific test to run
-pipenv run pytest -n 2        --durations=0 -vv                 --disable-pytest-warning  -k 'test_google' -s tests/test002.py
-                              #TODO why this param hide print()
+if [[ ! -z "$only_google_test" ]]; then
+    # run only google test
+    #                 #of worker  #show elapsed                     #turn off warning         #specific test to run
+    pipenv run pytest -n 2        --durations=0 -vv                 --disable-pytest-warning  -k 'test_google' -s tests/test002.py
+                                  #TODO why this param hide print()
+else
+    # run all tests
+    pipenv run pytest -n 2        --durations=0 -vv                 --disable-pytest-warning
+fi
